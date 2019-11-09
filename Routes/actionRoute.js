b3efb979ee8a21208727601267336e0834a1d528
Carlos Mitchell <kakismitchell@gmail.com>
2019-11-08 21:53:55 -0500

    Getting all Projects

--- Routes/actionRoute.js
+++ Routes/actionRoute.js
@@ -22,4 +22,64 @@ router.get("/:id", validateActionId, (req, res) => {
       );
   });
 
+  router.delete("/:id", validateActionId, (req, res) => {
+    Actions.remove(req.params.id)
+      .then(deleted => res.status(200).json({ recordsDeleted: deleted }))
+      .catch(() =>
+        res
+          .status(500)
+          .json({ errorMessage: "Error in removing specific project" })
+      );
+  });
+
+  router.put("/:id", [validateActionId, validateActionResource], (req, res) => {
+    Actions.update(req.params.id, req.body)
+      .then(update => res.status(202).json({ projectUpdated: update }))
+      .catch(() =>
+        res
+          .status(500)
+          .json({ errorMessage: "Error updating the specific Project" })
+      );
+  });
+
+//Middlewares
+
+function validateActionId(req, res, next) {
+    Actions.get(req.params.id)
+      .then(action => {
+        if (action) {
+          next();
+        } else {
+          res.status(400).json({ errorMessage: "Invalid Action ID" });
+        }
+      })
+      .catch(() =>
+        res.status(500).json({ errorMessage: "Error with accessing Actions" })
+      );
+  }
+
+  function validateActionResource(req, res, next) {
+    if (req.body.project_id === undefined) {
+      res.status(400).json({
+        errorMessage: "Make sure your action has a project_id"
+      });
+    } else if (req.body.description === undefined) {
+      res.status(400).json({
+        errorMessage: "Make sure your action has description field"
+      });
+    } else if (req.body.notes === undefined) {
+      res
+        .status(404)
+        .json({ errorMessage: "Make sure your action has a notes field" });
+    } else if (req.body.description.length > 128) {
+      res.status(404).json({
+        errorMessage:
+          "Woah there, description has too many characters, keep it under 128 characters"
+      });
+    } else {
+      next();
+    }
+  }
+  
+  module.exports = router;
   
--- Routes/projectRoute.js
+++ Routes/projectRoute.js
@@ -1,1 +1,14 @@
-1
+const express = require("express");
+
+const Project = require("../data/helpers/projectModel");
+const actionRouter = require("../Routes/actionRoute");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Project.get()
+      .then(project => res.status(200).json(project))
+      .catch(() => {
+        res.status(500).json({ errorMessage: "Couldn't retrieve all Projects" });
+      });
+  });
+  
