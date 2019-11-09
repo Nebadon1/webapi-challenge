9e35b3e78d67f76fca52d7088d6031d0acccc6cc
Carlos Mitchell <kakismitchell@gmail.com>
2019-11-08 21:45:14 -0500

    new action to a project

--- Routes/actionRoute.js
+++ Routes/actionRoute.js
@@ -3,3 +3,23 @@ const express = require("express");
 const Actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
+
+router.get("/:id", validateActionId, (req, res) => {
+    Actions.get(req.params.id)
+      .then(action => res.status(200).json(action))
+      .catch(() =>
+        res.status(500).json({ errorMessage: "Couldnt sent the action project" })
+      );
+  });
+
+  router.post("/", validateActionResource, (req, res) => {
+    Actions.insert(req.body)
+      .then(add => res.status(201).json({ Created: add }))
+      .catch(() =>
+        res
+          .status(500)
+          .json({ errorMessage: "Error with adding new action to database" })
+      );
+  });
+
+  
